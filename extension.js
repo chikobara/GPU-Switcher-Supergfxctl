1f265b83cb339d16be504b78e2454bea1a69913f
Chikobara <albaraa.as.2002@gmail.com>
2024-05-22 19:18:32 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   extension.js
    	modified:   metadata.json

--- extension.js
+++ extension.js
@@ -15,48 +15,90 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-import GObject from 'gi://GObject';
+import GObject from "gi://GObject";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import {
+  Extension,
+  gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
+import {
+  QuickToggle,
+  SystemIndicator,
+} from "resource:///org/gnome/shell/ui/quickSettings.js";
+import * as Util from "resource:///org/gnome/shell/misc/util.js";
 
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-import {QuickToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
-
-const ExampleToggle = GObject.registerClass(
-class ExampleToggle extends QuickToggle {
+const IntegratedToggle = GObject.registerClass(
+  class IntegratedToggle extends QuickToggle {
     constructor() {
-        super({
-            title: _('Smile'),
-            iconName: 'face-smile-symbolic',
-            toggleMode: true,
-        });
+      super({
+        title: _("Integrated"),
+        iconName: "computer-symbolic",
+        toggleMode: false,
+      });
+      this.connect("clicked", () => {
+        Util.spawnCommandLine("supergfxctl -m Integrated");
+      });
     }
-});
+  }
+);
 
-const ExampleIndicator = GObject.registerClass(
-class ExampleIndicator extends SystemIndicator {
+const HybridToggle = GObject.registerClass(
+  class HybridToggle extends QuickToggle {
     constructor() {
-        super();
-
-        this._indicator = this._addIndicator();
-        this._indicator.iconName = 'face-smile-symbolic';
-
-        const toggle = new ExampleToggle();
-        toggle.bind_property('checked',
-            this._indicator, 'visible',
-            GObject.BindingFlags.SYNC_CREATE);
-        this.quickSettingsItems.push(toggle);
+      super({
+        title: _("Hybrid"),
+        iconName: "processor-symbolic",
+        toggleMode: false,
+      });
+      this.connect("clicked", () => {
+        Util.spawnCommandLine("supergfxctl -m Hybrid");
+      });
     }
-});
-
-export default class QuickSettingsExampleExtension extends Extension {
-    enable() {
-        this._indicator = new ExampleIndicator();
-        Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
+  }
+);
+/* 
+const DedicatedToggle = GObject.registerClass(
+  class DedicatedToggle extends QuickToggle {
+    constructor() {
+      super({
+        title: _("Dedicated"),
+        iconName: "graphics-card-symbolic",
+        toggleMode: false,
+      });
+      this.connect("clicked", () => {
+        Util.spawnCommandLine("supergfxctl -m dedicated");
+      });
     }
+  }
+); */
 
-    disable() {
-        this._indicator.quickSettingsItems.forEach(item => item.destroy());
-        this._indicator.destroy();
+const GfxctlIndicator = GObject.registerClass(
+  class GfxctlIndicator extends SystemIndicator {
+    constructor() {
+      super();
+
+      this._indicator = this._addIndicator();
+      this._indicator.iconName = "graphics-card-symbolic";
+
+      const integratedToggle = new IntegratedToggle();
+      const hybridToggle = new HybridToggle();
+      // const dedicatedToggle = new DedicatedToggle();
+
+      this.quickSettingsItems.push(integratedToggle);
+      this.quickSettingsItems.push(hybridToggle);
+      // this.quickSettingsItems.push(dedicatedToggle);
     }
+  }
+);
+
+export default class GfxctlExtension extends Extension {
+  enable() {
+    this._indicator = new GfxctlIndicator();
+    Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
+  }
+
+  disable() {
+    this._indicator.quickSettingsItems.forEach((item) => item.destroy());
+    this._indicator.destroy();
+  }
 }
